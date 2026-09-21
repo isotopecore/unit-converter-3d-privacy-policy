aca293a8d0a6fa099d7087afd3b2b4a6f778a6be
UA-Ruslan <mnemonikus69@gmail.com>
2026-09-21 11:56:37 +0300

    Change base url in vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/convertify-privacy-policy/",
+  base: "/unit-converter-3d-privacy-policy/",
   plugins: [react()],
 });
